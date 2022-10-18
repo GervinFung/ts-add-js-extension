2f224194a4e7b1a4d73cf2f965acc264dabadb5d
GervinFung <gervinfungdaxuen@gmail.com>
2022-10-19 01:07:50 +0800

    test(package): use `cases-of-test` to wrap tests

--- test/index.ts
+++ test/index.ts
@@ -1,15 +1,7 @@
 import testParseConfig from './config';
 import testTsAddJsExtension from './process';
+import testCases from 'cases-of-test';
 
-const tests: ReadonlyArray<Readonly<[() => void, 'only'?]>> = [
-    [testParseConfig],
-    [testTsAddJsExtension],
-];
-
-const selectedTests = tests.filter(([_, only]) => only);
-
-if (process.env.IS_CI && selectedTests.length) {
-    throw new Error('cannot have "only" in ci cd');
-}
-
-(!selectedTests.length ? tests : selectedTests).forEach(([test]) => test());
+testCases({
+    tests: [[testParseConfig], [testTsAddJsExtension]],
+});
