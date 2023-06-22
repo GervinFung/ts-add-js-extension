1bf6692844a03cc5d63104035881af6234b36142
PoolOfDeath20 <53143214+GervinFung@users.noreply.github.com>
2023-06-22 23:30:26 +0800

    Set space same as assign in args (#47)
    * fix(args): let ` ` to be the same as `=`
    
    * chore(test/args): remove unnecessary log

--- test/args/operation.test.ts
+++ test/args/operation.test.ts
@@ -2,35 +2,43 @@ import { describe, it, expect } from 'vitest';
 import ParseArgs from '../../src/cli-command-parser';
 
 describe('Operation arguments parsing', () => {
-    it('should parse config when all options are given and "add" is absent', () => {
-        expect(
-            ParseArgs.create([
-                'node',
-                'ts-add-js-extension',
-                '--dir=build/mjs',
-                '--include=build/dts',
-                '--showchanges=true',
-            ]).asOperation()
-        ).toStrictEqual({
-            dir: 'build/mjs',
-            include: ['build/dts'],
-            showChanges: true,
-        });
-    });
-    it('should parse config when optional options are absent', () => {
-        expect(
-            ParseArgs.create([
-                'node',
-                'ts-add-js-extension',
-                'add',
-                '--dir=build/mjs',
-            ]).asOperation()
-        ).toStrictEqual({
-            dir: 'build/mjs',
-            include: undefined,
-            showChanges: undefined,
-        });
-    });
+    const delimiter = ['=', ' '] as const;
+
+    it.each(delimiter)(
+        'should parse config when all options are given and "add" is absent',
+        (delimiter) => {
+            expect(
+                ParseArgs.create([
+                    'node',
+                    'ts-add-js-extension',
+                    `--dir${delimiter}build/mjs`,
+                    `--include${delimiter}build/dts`,
+                    `--showchanges${delimiter}true`,
+                ]).asOperation()
+            ).toStrictEqual({
+                dir: 'build/mjs',
+                include: ['build/dts'],
+                showChanges: true,
+            });
+        }
+    );
+    it.each(delimiter)(
+        'should parse config when optional options are absent',
+        (delimiter) => {
+            expect(
+                ParseArgs.create([
+                    'node',
+                    'ts-add-js-extension',
+                    'add',
+                    `--dir${delimiter}build/mjs`,
+                ]).asOperation()
+            ).toStrictEqual({
+                dir: 'build/mjs',
+                include: undefined,
+                showChanges: undefined,
+            });
+        }
+    );
     it('should parse config and throw error when mandatory options are absent', () => {
         expect(() =>
             ParseArgs.create([
