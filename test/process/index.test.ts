08b297adf5b346fd6f60c035eed76b719749db59
GervinFung <gervinfungdaxuen@gmail.com>
2023-07-06 21:52:08 +0800

    test(process): finalize assertion

--- test/process/index.test.ts
+++ test/process/index.test.ts
@@ -8,38 +8,91 @@ describe('ts add js extension', () => {
     const getPath = (subPath: string) => path.join(__dirname, subPath);
 
     beforeAll(() => {
-        const testSetup = 'cd test/process && shx cp -r -f source actual-result && pnpm prebuild'
+        const testSetup =
+            'cd test/process && shx cp -rf source actual-result && pnpm prebuild';
+
         console.log({
             output: child.execSync(testSetup).toString(),
         });
     });
-    it('should be able to append .js/.mjs extension for JavaScript file', async () => {
-        const result = await tsAddJsExtension({
+
+    describe('for JavaScript files only', () => {
+        const dir = getPath(path.join('actual-result', 'js'));
+
+        const result = tsAddJsExtension({
             config: {
-                dir: getPath(path.join('actual-result', 'js')),
+                dir,
             },
         });
-        expect(result.type).toBe('done');
-        // actual result = expected result
-        expect(result.type).toBe('done');
+
+        it('should be able to append either js or mjs file extension', async () => {
+            expect((await result).type).toBe('done');
+        });
+
+        it.each(
+            fs
+                .readdirSync(dir)
+                .map((file) => path.join(dir, file))
+                .map((file) => ({
+                    file,
+                    content: fs.readFileSync(file, { encoding: 'utf-8' }),
+                }))
+        )(
+            'should assert that file extension was added to proper import/export statement',
+            ({ file, content }) => {
+                expect(content).toBe(
+                    fs.readFileSync(file.replace('actual', 'expected'), {
+                        encoding: 'utf-8',
+                    })
+                );
+            }
+        );
     });
-    it.each(
-        (() => {
-            const parentDir = getPath(path.join('actual-result', 'dts'));
-            return fs
+
+    describe('for Type Definition files only', () => {
+        const parentDir = getPath(path.join('actual-result', 'dts'));
+
+        describe.each(
+            fs
                 .readdirSync(parentDir)
-                .map((childPath) => path.join(parentDir, childPath));
-        })()
-    )(
-        'should be able to append .js/.mjs extension for Type Definition file of %s',
-        async (dir) => {
-            const result = await tsAddJsExtension({
-                config: {
-                    dir,
-                },
-            });
-            // actual result = expected result
-            expect(result.type).toBe('done');
-        }
-    );
+                .map((childPath) => path.join(parentDir, childPath))
+        )(
+            'assert that it will work for Type Definition files with or without JavaScript',
+            (dir) => {
+                const result = tsAddJsExtension({
+                    config: {
+                        dir,
+                    },
+                });
+
+                it(`should be able to append .js/.mjs extension for Type Definition file of ${dir}`, async () => {
+                    expect((await result).type).toBe('done');
+                });
+
+                it.each(
+                    fs
+                        .readdirSync(dir)
+                        .map((file) => path.join(dir, file))
+                        .map((file) => ({
+                            file,
+                            content: fs.readFileSync(file, {
+                                encoding: 'utf-8',
+                            }),
+                        }))
+                )(
+                    'should assert that file extension was added to proper import/export statement',
+                    ({ file, content }) => {
+                        expect(content).toBe(
+                            fs.readFileSync(
+                                file.replace('actual', 'expected'),
+                                {
+                                    encoding: 'utf-8',
+                                }
+                            )
+                        );
+                    }
+                );
+            }
+        );
+    });
 });
