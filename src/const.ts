eddef8057d6dc892f7f67399b8f61492616c97c4
GervinFung <gervinfungdaxuen@gmail.com>
2023-10-12 17:59:27 +0800

    fix(filepath): parse the file path of code for `.js` and `.mjs` case, removed `.js` case for `import`/`export` statement

--- src/const.ts
+++ src/const.ts
@@ -2,22 +2,24 @@ class ExtensionsUtil {
 	static readonly extensions = {
 		javaScript: {
 			js: '.js',
+			mjs: '.mjs',
 		},
 		typeDefinition: {
 			dts: '.d.ts',
+			dmts: '.d.mts',
 		},
 	} as const;
 
 	static readonly matchJs = (filePath: string) => {
-		const { js } = this.extensions.javaScript;
+		const { js, mjs } = this.extensions.javaScript;
 
-		return filePath.endsWith(js);
+		return filePath.endsWith(js) || filePath.endsWith(mjs);
 	};
 
 	static readonly matchDts = (filePath: string) => {
-		const { dts } = this.extensions.typeDefinition;
+		const { dts, dmts } = this.extensions.typeDefinition;
 
-		return filePath.endsWith(dts);
+		return filePath.endsWith(dts) || filePath.endsWith(dmts);
 	};
 
 	static readonly matchEither = (filePath: string) => {
