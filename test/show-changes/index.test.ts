65922f3557aad38204de7e31064362cdba30c781
PoolOfDeath20 <53143214+GervinFung@users.noreply.github.com>
2024-06-21 11:32:26 +0800

    test(*): added 1 gap between each test code in relevant file (#92)

--- test/show-changes/index.test.ts
+++ test/show-changes/index.test.ts
@@ -5,6 +5,7 @@ describe('Log', () => {
 	it('should create Log instance when there is at least one file to be changed', () => {
 		expect(Log.fromNumberOfFiles(1)).toBeInstanceOf(Log);
 	});
+
 	it('should throw error when there is an attempt to instantiate Log but there is no file to be changed', () => {
 		expect(() => {
 			return Log.fromNumberOfFiles(0);
