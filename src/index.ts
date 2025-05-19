56dfaa3a6f162efbfce747e51f4f0cc230c58cc9
GervinFung <gervinfungdaxuen@gmail.com>
2025-05-19 13:02:13 +0800

    refactor(src): refactored to better variable naming

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,37 @@ import type { PartialConfig } from './cli-command-parser';
 import type { ParsedConfig } from './config';
 
 import ParseArgs from './cli-command-parser';
-import { parseConfig, valuelizeConfig } from './config';
+import { parseConfig, normaliseConfig } from './config';
 import { writeMany, findMany } from './read-write';
 
-const tsAddJsExtension = async ({
-	config,
+const tsAddJsExtension = async (
+	props: Readonly<{
+		config: PartialConfig;
+		/**
+		 * @deprecated since version 1.4.0
+		 * Will be removed in version 2.0
+		 * To pass configurations to `tsAddJsExtension`
+		 * Just pass the configurations directly
+		 * ```
+		 * tsAddJsExtension({
+		 *  config:{
+		 *   dir:'dist'
+		 *  }
+		 * })
+		 * ```
+		 * Instead of passing with this function
+		 * ```
+		 * tsAddJsExtension({
+		 *  parsedConfigFunction: () => parseConfig(argv)
+		 * })
+		 * ```
+		 * As it will be ignroed
+		 */
+		parsedConfigFunction?: () => ParsedConfig;
+	}>
+) => {
 	// eslint-disable-next-line @typescript-eslint/no-deprecated
-	parsedConfigFunction,
-}: Readonly<{
-	config: PartialConfig;
-	/**
-	 * @deprecated since version 1.4.0
-	 * Will be deleted in version 2.0
-	 * To pass configurations to `tsAddJsExtension`
-	 * Just pass the configurations directly
-	 * ```
-	 * tsAddJsExtension({
-	 *  config:{
-	 *   dir:'dist'
-	 *  }
-	 * })
-	 * ```
-	 * Instead of passing with this function
-	 * ```
-	 * tsAddJsExtension({
-	 *  parsedConfigFunction: () => parseConfig(argv)
-	 * })
-	 * ```
-	 * As it will be ignroed
-	 */
-	parsedConfigFunction?: () => ParsedConfig;
-}>) => {
-	if (parsedConfigFunction) {
+	if (props.parsedConfigFunction) {
 		throw new Error(
 			[
 				`Please do not use parsedConfigFunction as it's deprecated and contains complicated parsing`,
@@ -42,11 +41,11 @@ const tsAddJsExtension = async ({
 		);
 	}
 
-	const trueConfig = valuelizeConfig(config);
+	const normalisedConfig = normaliseConfig(props.config);
 
 	return writeMany({
-		showChanges: trueConfig.showChanges,
-		foundMany: await findMany(trueConfig),
+		showProgress: normalisedConfig.showProgress,
+		foundMany: await findMany(normalisedConfig),
 	});
 };
 
